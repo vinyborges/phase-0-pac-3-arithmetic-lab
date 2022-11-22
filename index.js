aa27618467e2a2f3edab779f5da8fdbb6e7670c9
vinyborges <viny.bdj@gmail.com>
2022-11-22 03:30:56 -0800

    commit tree f7c429cee36b1fa7380371bc06f0bc18a9860688

---
+++ index.js
@@ -0,0 +1,41 @@
+//basic functions...
+
+function add(a,b){
+
+    return a+b;
+}
+function subtract(a,b){
+
+    return a-b;
+}
+function multiply(a,b){
+
+    return a*b;
+}
+function divide(a,b){
+
+    return a/b;
+}
+function increment(n){
+
+    return ++n;
+}
+function decrement(n){
+
+    return --n;
+}
+
+
+//PARSE INT 
+
+function makeInt(n){
+    
+return parseInt(n, 10);
+}
+
+// PARSE FLOAT
+
+function preserveDecimal(n){
+    
+    return parseFloat(n, 10);
+    }
